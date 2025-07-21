2c21d465129103de46fcb817c6926074b08f3e6d
Anthony Sandoval <anthony.sandoval.dev@gmail.com>
2025-07-21 10:18:26 +0100

    style: added background image to hero section

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -5,7 +5,6 @@ import { CategoryBox } from "@/ui/category-box";
 import { ProductList } from "@/ui/products/product-list";
 import { YnsLink } from "@/ui/yns-link";
 import * as Commerce from "commerce-kit";
-import Image from "next/image";
 import type { Metadata } from "next/types";
 
 export const metadata = {
@@ -18,7 +17,7 @@ export default async function Home() {
 
 	return (
 		<main>
-			<section className="rounded bg-neutral-100 py-8 sm:py-12">
+			<section className="rounded py-8 sm:py-12 bg-[url('/images/hero-img.webp')] bg-cover bg-center">
 				<div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
 					<div className="max-w-md space-y-4">
 						<h2 className="text-balance text-3xl font-bold tracking-tight md:text-4xl">{t("hero.title")}</h2>
@@ -30,19 +29,6 @@ export default async function Home() {
 							{t("hero.action")}
 						</YnsLink>
 					</div>
-					<Image
-						alt="Cup of Coffee"
-						loading="eager"
-						priority={true}
-						className="rounded"
-						height={450}
-						width={450}
-						src="https://files.stripe.com/links/MDB8YWNjdF8xT3BaeG5GSmNWbVh6bURsfGZsX3Rlc3RfaDVvWXowdU9ZbWlobUIyaHpNc1hCeDM200NBzvUjqP"
-						style={{
-							objectFit: "cover",
-						}}
-						sizes="(max-width: 640px) 70vw, 450px"
-					/>
 				</div>
 			</section>
 
